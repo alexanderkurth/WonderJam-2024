13f281d6bab4db21352ad5fb9838a066d6fa6d15
Florine Carlier <florine.carlier17@gmail.com>
2024-03-02 13:40:44 -0500

    Fix break

--- Assets/Sources/MontureController.cs
+++ Assets/Sources/MontureController.cs
@@ -43,6 +43,7 @@ public class MontureController : MonoBehaviour
             {
                 slot.SetBodyPart(type);
                 NbSlotsEquipped++;
+                break; 
             }
         }
     }
