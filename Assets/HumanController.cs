af8bf04c787c92a7b693482dfd726dff939768b0
Alexander Kurth <alexandrekurth@outlook.com>
2024-03-02 20:35:18 -0500

    ddd

--- Assets/HumanController.cs
+++ Assets/HumanController.cs
@@ -9,16 +9,6 @@ using DG.Tweening;
 
 public class HumanController : MonoBehaviour
 {
-    public enum InteractionState : Int32
-    {
-        Idle = 0,
-        Grab = 1,
-        Merge = 2,
-
-        Count = 3,
-        Invalid = 4
-    }
-
     [SerializeField] private float radius = 10.0f;
     [SerializeField] private float radiusSaddle = 10.0f;
     public float MovementSpeed = 5.0f;
@@ -27,7 +17,6 @@ public class HumanController : MonoBehaviour
     [SerializeField] private GameObject _cameraRoot;
     [SerializeField] private InteractionComponent2 _interactionComponent;
     private TeamID _teamID;
-    public InteractionState m_State = InteractionState.Invalid;
     public int DashDistance = 1;
     private void Start()
     {
@@ -100,20 +89,18 @@ public class HumanController : MonoBehaviour
         MontureController mc = _interactionComponent.bestSaddle;
         float distanceSaddle = Vector3.Distance(mc.transform.position, transform.position);
 
-        if (distanceSaddle <= radiusSaddle && m_Sate == InteractionState.Grab)
+        if (distanceSaddle <= radiusSaddle)
         {
             ia.OnMerge(mc);
             ia = null;
-            m_Sate = InteractionState.Idle;
             return;
         }
 
         float distance = Vector3.Distance(ia.transform.position, transform.position);
-        if (m_Sate != InteractionState.Grab && distance <= radius)
+        if (distance <= radius)
         {
             if (ia != null && !ia.IsGrab)
             {
-                m_Sate = InteractionState.Grab;
                 ia.OnGrab();
                 ia.transform.parent = transform;
                 ia.transform.localScale *= 0.75f;
